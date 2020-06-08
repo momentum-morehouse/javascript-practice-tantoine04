d3f5a5a29ea640e7829b1d63d482599c27f9db1e
tantoine04 <tantoine04@gmail.com>
2020-06-08 13:06:06 +0000

    answersto homework question

--- homework.js
+++ homework.js
@@ -6,46 +6,92 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove(nameArray, potential) {
+  nameArray = nameArray.slice()
+  let idx = nameArray.indexOf(potential)
+  console.log(idx)
+  let newArray = []
+  for (let name of nameArray) {
+    if (name !== potential) {
+      newArray.push(name)
+    }
+
+  }
+  return newArray
+}
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
-
+function sum(numbers) {
+  let total = 0;
+  if (numbers.length > 0) {
+    for (let number of numbers) {
+      total += number;
+    }
+  }
+  return total;
+}
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(numbers) {
+  if (numbers.length > 0) {
+    let average = sum(numbers) / numbers.length;
+    return average;
+  }
+}
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum(numbers){
+  let min = 0;
+  for (let number of numbers){
+    if (0 < numbers.length) {
+      let min = numbers.length
+    }
+    return min;
+  }
+}
 
-// 6. There are many techniques to sort arrays in programming. Your programming
-// language will likely include the ability to do this. We are going to
-// implement sorting ourselves, however.
-//
-// A "selection sort" is one of the most simple sorting algorithms. To implement it,
-// you start with an unsorted array of numbers. You search the array and find the
-// smallest number in the array. You then insert that into a new array as the first
-// element and remove it from the original array. You continue doing this until
-// there are no numbers left in the original array.
-//
-// Create a function called selectionSort that takes an array of numbers and returns
-// a sorted array using the above technique.
-//
-// Note 1: You do not actually want to delete things from the original array. You
-// should copy it first. To copy an array, use the following code:
-//
-// let arrayCopy = array.slice(0);
-//
-// Think about why this works.
-//
-// Note 2: Selection sort can be implemented using one array. Read the explanation at
-// https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
-// to see how. This may make more sense to you.
 
-// 7. Create a function called `textList` that takes an array and joins its elements
-// into a string separated by commas.
-//
-// For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
-// `"Cadence,Ordel,Marion"`.
+  // 6. There are many techniques to sort arrays in programming. Your programming
+  // language will likely include the ability to do this. We are going to
+  // implement sorting ourselves, however.
+  //
+  // A "selection sort" is one of the most simple sorting algorithms. To implement it,
+  // you start with an unsorted array of numbers. You search the array and find the
+  // smallest number in the array. You then insert that into a new array as the first
+  // element and remove it from the original array. You continue doing this until
+  // there are no numbers left in the original array.
+  //
+  // Create a function called selectionSort that takes an array of numbers and returns
+  // a sorted array using the above technique.
+  //
+  // Note 1: You do not actually want to delete things from the original array. You
+  // should copy it first. To copy an array, use the following code:
+  //
+  // let arrayCopy = array.slice(0);
+  //
+  // Think about why this works.
+  //
+  // Note 2: Selection sort can be implemented using one array. Read the explanation at
+  // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
+  // to see how. This may make more sense to you.
+   function selectionSort(numbers) {
+    
+    let numbersCopy = numbers.slice();
+
+    let sorted = [];
+
+  
+  }
+
+  // 7. Create a function called `textList` that takes an array and joins its elements
+  // into a string separated by commas.
+  //
+  // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
+  // `"Cadence,Ordel,Marion"`.
+ 
 
